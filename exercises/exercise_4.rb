29cc0e15552f87ef9c6de3c0e01ef9bd1a5efe5a
max k <maxkuhn@gmail.com>
2022-08-22 22:30:53 -0700

    Adds answers through ex 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Failtown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where("mens_apparel = 'true'")
+
+@womens_stores = Store.where("womens_apparel = 'true' and annual_revenue < 1000000")
+
+@mens_stores.each do | boystore |
+  puts "The #{boystore.name} store makes $#{boystore.annual_revenue} annually."
+end
+
+@womens_stores.each do | girlstore |
+  puts "The #{girlstore.name} store makes $#{girlstore.annual_revenue} annually."
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+@avg_revenue = @total_revenue / Store.count
+@million_sellers = Store.where("annual_revenue > 1000000").count
+puts "Total revenue for entire company is $#{@total_revenue}."
+puts "Average revenue is $#{@avg_revenue}."
+puts "There are #{@million_sellers} stores making over a million in sales."
+puts "These are the stolen fruits of the workers' labour."
